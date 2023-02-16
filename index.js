e5fec827702240c5c9c39a57e13dc6aaefd11183
Sebastian Romero <nakosume@gmail.com>
2023-02-16 09:18:13 -0500

    Added cowsayjs package

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ng = require('name-generate');
+const cs = require('cowsayjs');
 
 let n1=ng.first();
 let l1=ng.last();
@@ -6,5 +7,13 @@ let l1=ng.last();
 let n2=ng.first();
 let l2=ng.last();
 
-console.log(`${n1} ${l1}`);
-console.log(`${n2} ${l2}`);
+let fn1=n1+" "+l1;
+let fn2=n2+" "+l2;
+
+console.log(cs.moo(`mi nombre es ${fn1}`,{
+    cow:"dragon"
+}));
+
+console.log(cs.moo(`y el mio es ${fn2}`,{
+    cow:"skeleton"
+}));
